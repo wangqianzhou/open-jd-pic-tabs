01d76ba39de22ed68853c4cc6572eb089119007e
wangqianzhou <wangqianzhou@foxmail.com>
2017-09-02 14:24:51 +0800

    Initial Commit

---
+++ open-jd-pic-tabs/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  open-jd-pic-tabs
+//
+//  Created by 王乾舟 on 02/09/2017.
+//  Copyright © 2017 王乾舟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
